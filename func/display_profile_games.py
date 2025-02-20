9b7f5fe55576acc17b4f3fa367976fbe074b22bd
mrDark315 <ashykhmin04@gmail.com>
2025-02-21 01:28:05 +0200

    Add label games not found

--- func/display_profile_games.py
+++ func/display_profile_games.py
@@ -1,5 +1,6 @@
 import json
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QLabel
+from PyQt5 import QtCore
 from components.GameCardProfile import GameCardProfile
 
 def display_profile_games(ui: QWidget):
@@ -23,24 +24,32 @@ def display_profile_games(ui: QWidget):
             widget.deleteLater()
 
     # Get the required games for the current page
-    start_index = ui.current_page * 6
-    end_index = start_index + 6
-    games_to_display = ui.saved_games[start_index:end_index]
+    total_games = len(ui.saved_games)
+    if total_games == 0:
+        no_games_label = QLabel("No games found")
+        no_games_label.setStyleSheet("color: #454C55; font-size: 24px; font-weight: bold;")
+        # no_games_label.setContentsMargins(400, 0, 200, 0)
+        no_games_label.setAlignment(QtCore.Qt.AlignCenter)
+        ui.grid_layout.addWidget(no_games_label, 0, 0, 1, 3)
+    else:
+        start_index = ui.current_page * 6
+        end_index = start_index + 6
+        games_to_display = ui.saved_games[start_index:end_index]
 
-    row, col = 0, 0
-    max_cols = 3
+        row, col = 0, 0
+        max_cols = 3
 
-    for game in games_to_display:
-        game_widget = GameCardProfile(game, ui)
-        ui.grid_layout.addWidget(game_widget, row, col)
+        for game in games_to_display:
+            game_widget = GameCardProfile(game, ui)
+            ui.grid_layout.addWidget(game_widget, row, col)
 
-        col += 1
-        if col >= max_cols:
-            col = 0
-            row += 1
+            col += 1
+            if col >= max_cols:
+                col = 0
+                row += 1
+
+        print(f"🎮 Отображаемые игры в профиле: {[game['name'] for game in games_to_display]}")
 
     # Turn navigation buttons on/off
     ui.left_arrow.setEnabled(ui.current_page > 0)
     ui.right_arrow.setEnabled((ui.current_page + 1) * 6 < len(ui.saved_games))
-
-    print(f"🎮 Отображаемые игры в профиле: {[game['name'] for game in games_to_display]}")
